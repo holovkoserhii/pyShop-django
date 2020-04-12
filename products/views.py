8e54fbf54db5d062e318f1c82b983b1343b5585a
Serhii Holovko <holovko.s@gmail.com>
2020-04-12 23:48:07 +0300

    initial commit

---
+++ products/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
+
+# /products -> index
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {
+        'products': products
+    })
+
+
+def new_products(request):
+    return HttpResponse('This is new products!!!11')
